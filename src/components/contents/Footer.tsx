2d4b360b2fbbe47ef20e49a896eaf5bb63d06953
Levente <168279599+tothlevente@users.noreply.github.com>
2024-12-16 14:25:48 +0000

    Develop footer

--- src/components/contents/Footer.tsx
+++ src/components/contents/Footer.tsx
@@ -1,34 +1,51 @@
 import packageJson from "../../../package.json";
 
+import ExternalLink from "../icons/external-link";
+
+import { Button } from "../ui/button";
+
 export default function Footer() {
   return (
-    <footer>
-      <div className="footer-text">
-        <p>
-          This website under MIT license, ad-free and does not use cookies.
-        </p>
-        <p>For more information please visit the project repository.</p>
-        <p>Created by Levente in 2024</p>
-        <p>v{packageJson.version}</p>
+    <div className="footer">
+      <p>This website is open source and licensed under MIT.</p>
+      <p>
+        This website is does not use cookies and collect data about you.
+      </p>
+      <p>For more information please visit the project repository.</p>
+      <p>Thank you for your visiting and intrest! 👍️</p>
+      <p>Created by Levente in 2024 | v{packageJson.version}</p>
+      <div className="footer-link">
+        <Button asChild>
+          <a
+            href="https://github.com/tothlevente/to-do-list"
+            target="_blank"
+            style={{ marginRight: "8px" }}
+          >
+            <ExternalLink />
+            Repository
+          </a>
+        </Button>
+        <Button asChild>
+          <a
+            href="https://github.com/tothlevente/to-do-list/blob/main/LICENSE"
+            target="_blank"
+            style={{ marginRight: "8px" }}
+          >
+            <ExternalLink />
+            License
+          </a>
+        </Button>
       </div>
-      <div>
-        <a
-          className="link"
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/tothlevente"
-        >
-          <i className="bi bi-person-circle"></i>
-        </a>
-        <a
-          className="link"
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/tothlevente/to-do-list"
-        >
-          <i className="bi bi-github"></i>
-        </a>
-      </div>
-    </footer>
+      <a
+        href="https://www.netlify.com"
+        target="_blank"
+        style={{ paddingBottom: "10px" }}
+      >
+        <img
+          src="https://www.netlify.com/v3/img/components/netlify-color-accent.svg"
+          alt="Deploys by Netlify"
+        />
+      </a>
+    </div>
   );
 }
