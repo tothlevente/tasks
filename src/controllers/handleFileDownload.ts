f96e331220f1e98e9351710bef24ad57723056fe
Levente <168279599+tothlevente@users.noreply.github.com>
2024-12-17 17:29:47 +0000

    Develop handleFileDownload.ts

---
+++ src/controllers/handleFileDownload.ts
@@ -0,0 +1,24 @@
+import { TodoProps } from "@/interfaces/TodoProps";
+
+export default function handleFileDownload(value: TodoProps[]) {
+  const separator = ", ";
+  let result = "";
+
+  for (let i = 0; i < value.length; i++) {
+    console.log(value[i].title);
+
+    result += value[i].title;
+    if (i !== value.length - 1) {
+      result += separator;
+    }
+  }
+
+  const element = document.createElement("a");
+  const file = new Blob([result], {
+    type: "text/plain",
+  });
+  element.href = URL.createObjectURL(file);
+  element.download = "myFile.txt";
+  document.body.appendChild(element);
+  element.click();
+}
