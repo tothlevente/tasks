b2f06489280fb15c22b6f4e2c5d3f76269143532
Levente <168279599+tothlevente@users.noreply.github.com>
2024-12-17 17:29:28 +0000

    Develop TodoList.tsx components

--- src/components/contents/TodoList.tsx
+++ src/components/contents/TodoList.tsx
@@ -1,11 +1,7 @@
-import { TodoProps } from "@/interfaces/TodoProps";
-import { Button } from "../ui/button";
+import TodoListNoContent from "../todo-list-no-content";
+import TodoListContent from "../todo-list-content";
 
-import CircleCheckMin from "../icons/circle-check-min";
-import CircleCheck from "../icons/circle-check";
-import CreatedAt from "../created-at";
-import Trash from "../icons/trash";
-import Copy from "../icons/copy";
+import { TodoProps } from "@/interfaces/TodoProps";
 
 export default function TodoList({
   list,
@@ -20,75 +16,16 @@ export default function TodoList({
 }) {
   return (
     <div className="todo-list">
-      <div className="todo-list-content">
-        {list.map((value, index) => (
-          <div
-            key={index}
-            className="todo-card"
-          >
-            <div className="todo-card-header">
-              <p
-                className={
-                  value.completed
-                    ? "todo-card-title completed"
-                    : "todo-card-title"
-                }
-              >
-                {value.title}
-              </p>
-            </div>
-            <div className="todo-card-footer">
-              <div className={value.completed ? "completed" : ""}>
-                <CreatedAt value={value.createdAt} />
-              </div>
-              <div>
-                {value.completed ? (
-                  <Button
-                    variant="outline"
-                    size="icon"
-                    className="todo-card-footer-button circle-check-button"
-                    onClick={() => {
-                      toggleCompleteTodo(value.id);
-                    }}
-                  >
-                    <CircleCheck />
-                  </Button>
-                ) : (
-                  <Button
-                    variant="outline"
-                    size="icon"
-                    className="todo-card-footer-button"
-                    onClick={() => {
-                      toggleCompleteTodo(value.id);
-                    }}
-                  >
-                    <CircleCheckMin />
-                  </Button>
-                )}
-
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="todo-card-footer-button"
-                  onClick={() => {
-                    copyTodo(value.id);
-                  }}
-                >
-                  <Copy />
-                </Button>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="todo-card-footer-button"
-                  onClick={() => deleteTodo(value.id)}
-                >
-                  <Trash />
-                </Button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      {list.length === 0 ? (
+        <TodoListNoContent />
+      ) : (
+        <TodoListContent
+          list={list}
+          toggleCompleteTodo={toggleCompleteTodo}
+          copyTodo={copyTodo}
+          deleteTodo={deleteTodo}
+        />
+      )}
     </div>
   );
 }
---
+++ src/components/todo-list-no-content.tsx
@@ -0,0 +1,15 @@
+export default function TodoListNoContent() {
+  return (
+    <div className="todo-list-no-content">
+      <p>
+        <b>There is no any content yet.</b>
+      </p>
+      <p>Please add some content first.</p>
+      <p>Your content not saved to your browser currently!</p>
+      <p>
+        If you want to save your content please turn on this feature in the
+        settings.
+      </p>
+    </div>
+  );
+}
