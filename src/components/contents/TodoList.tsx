a99d83c35be1b2c3275b71e61cb2c39a2a944356
Levente <168279599+tothlevente@users.noreply.github.com>
2024-12-17 09:01:40 +0000

    Develop TodoList.tsx components

--- src/components/contents/TodoList.tsx
+++ src/components/contents/TodoList.tsx
@@ -1,11 +1,38 @@
 import { TodoProps } from "@/interfaces/TodoProps";
+import { Button } from "../ui/button";
+
+import Pencil from "../icons/pencil";
+import Trash from "../icons/trash";
 
 export default function TodoList({ list }: { list: TodoProps[] }) {
   return (
     <div className="todo-list">
-      {list.map((value, index) => (
-        <div key={index}>{value.title}</div>
-      ))}
+      <div className="todo-list-content">
+        {list.map((value, index) => (
+          <div
+            key={index}
+            className="todo-card"
+          >
+            <div className="repository-card-header">
+              <p className="repository-card-title">{value.title}</p>
+            </div>
+            <div className="repository-card-footer">
+              <Button
+                variant="default"
+                size="icon"
+              >
+                <Pencil />
+              </Button>
+              <Button
+                variant="destructive"
+                size="icon"
+              >
+                <Trash />
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
