3eabcce97f17d53385b525f7b000641ffb83b168
Levente <168279599+tothlevente@users.noreply.github.com>
2024-12-19 08:29:31 +0000

    Develop settings

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -1,12 +1,10 @@
 import {
   DropdownMenu,
-  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuSub,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
@@ -16,7 +14,6 @@ import { Button } from "./ui/button";
 
 import handleFileDownload from "@/controllers/handleFileDownload";
 import CircleSettings from "./icons/circle-settings";
-import FloppyDisk from "./icons/floppy-disk";
 import Download from "./icons/download";
 import Trash from "./icons/trash";
 
@@ -41,27 +38,6 @@ export default function Settings({
       <DropdownMenuContent className="w-56">
         <DropdownMenuLabel>Settings</DropdownMenuLabel>
         <DropdownMenuSeparator />
-
-        <DropdownMenuSub>
-          <DropdownMenuSubTrigger>
-            <FloppyDisk />
-            <span>Save content and settings in this browser</span>
-          </DropdownMenuSubTrigger>
-          <DropdownMenuContent className="w-56">
-            <DropdownMenuCheckboxItem
-              checked={true}
-              onCheckedChange={() => {}}
-            >
-              Save my content and settings in this browser
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuCheckboxItem
-              checked={false}
-              onCheckedChange={() => {}}
-            >
-              Do not save anything
-            </DropdownMenuCheckboxItem>
-          </DropdownMenuContent>
-        </DropdownMenuSub>
         <DropdownMenuSub>
           <DropdownMenuItem
             disabled={list.length === 0}
