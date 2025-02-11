a42d1166efbff4e2eea555c68e1155b10022bec3
Levente <168279599+tothlevente@users.noreply.github.com>
2025-02-11 07:48:29 +0000

    Develop header components

--- src/components/icons/notepad-text.tsx
+++ src/components/icons/notepad-text.tsx
@@ -2,8 +2,8 @@ export default function NotepadText() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="34"
-      height="34"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
