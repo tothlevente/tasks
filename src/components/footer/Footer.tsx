416b5ce6c813048ed544b6f55eee58c1a10b22ae
Levente <168279599+tothlevente@users.noreply.github.com>
2025-03-27 11:12:53 +0000

    Refactor Footer component for improved layout and styling; replace div with footer element and update CSS for better responsiveness

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,54 +1,67 @@
 import { ExternalLinkIcon } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import { Button } from "../ui/button";
-
 import packageJson from "../../../package.json";
 
 export const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="footer">
-      <p>{t("footerLineLicensed")}</p>
-      <p>{t("footerLineNoCookies")}</p>
-      <p>{t("footerLineUseLocalStorage")}</p>
-      <p>{t("footerLineMoreInformation")}</p>
-      <p>{t("footerLineThankYou")}</p>
-      <p>
-        {t("footerLineCreatedBy")} | v{packageJson.version}
-      </p>
-      <div className="footer-link">
-        <Button asChild>
+    <footer className="p-6">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between">
+        <div className="mb-6 md:mb-0">
+          <p className="text-sm pb-1">{t("footerLineLicensed")}</p>
+          <p className="text-sm pb-1">{t("footerLineNoCookies")}</p>
+          <p className="text-sm pb-1">{t("footerLineUseLocalStorage")}</p>
+          <p className="text-sm pb-1">{t("footerLineMoreInformation")}</p>
+          <p className="text-sm pb-3">{t("footerLineThankYou")}</p>
+          <p className="text-sm">
+            {t("footerLineCreatedBy")} | v{packageJson.version}
+          </p>
+        </div>
+
+        <div className="flex flex-col items-start md:items-end">
+          <div className="flex flex-col space-y-2 mb-4">
+            <Button
+              variant="outline"
+              asChild
+            >
+              <a
+                href="https://github.com/tothlevente/to-do-list"
+                target="_blank"
+                className="flex items-center hover:underline text-sm"
+              >
+                <ExternalLinkIcon className="mr-2" />
+                {t("footerRepositoryLink")}
+              </a>
+            </Button>
+            <Button
+              variant="outline"
+              asChild
+            >
+              <a
+                href="https://github.com/tothlevente/to-do-list/blob/main/LICENSE"
+                target="_blank"
+                className="flex items-center hover:underline text-sm"
+              >
+                <ExternalLinkIcon className="mr-2" />
+                {t("footerLicenseLink")}
+              </a>
+            </Button>
+          </div>
           <a
-            href="https://github.com/tothlevente/to-do-list"
+            href="https://www.netlify.com"
             target="_blank"
-            style={{ marginRight: "8px" }}
+            className="mt-2"
           >
-            <ExternalLinkIcon />
-            {t("footerRepositoryLink")}
+            <img
+              src="https://www.netlify.com/v3/img/components/netlify-color-accent.svg"
+              alt="Deploys by Netlify"
+              className="w-32"
+            />
           </a>
-        </Button>
-        <Button asChild>
-          <a
-            href="https://github.com/tothlevente/to-do-list/blob/main/LICENSE"
-            target="_blank"
-            style={{ marginRight: "8px" }}
-          >
-            <ExternalLinkIcon />
-            {t("footerLicenseLink")}
-          </a>
-        </Button>
+        </div>
       </div>
-      <a
-        href="https://www.netlify.com"
-        target="_blank"
-        style={{ paddingBottom: "10px" }}
-      >
-        <img
-          src="https://www.netlify.com/v3/img/components/netlify-color-accent.svg"
-          alt="Deploys by Netlify"
-        />
-      </a>
-    </div>
+    </footer>
   );
 };
