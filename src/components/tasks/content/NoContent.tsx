9bbf008bb7acb3a105c969af5aa1d840760a832d
Levente <168279599+tothlevente@users.noreply.github.com>
2025-03-27 11:17:04 +0000

    Update NoContent component to enhance alert styling and improve accessibility

--- src/components/tasks/content/NoContent.tsx
+++ src/components/tasks/content/NoContent.tsx
@@ -10,7 +10,10 @@ export const NoContent = () => {
       <h4 className="scroll-m-20 text-xl font-bold tracking-tight">{t("noContentTitle")}</h4>
       <p>{t("noContentDescription")}</p>
       <div className="mt-4 w-[250px] sm:w-[450px]">
-        <Alert variant="destructive">
+        <Alert
+          variant="destructive"
+          className="dark:bg-red-400 drak:text-red-900 bg-red-100 text-red-900"
+        >
           <AlertCircleIcon className="h-4 w-4" />
           <AlertTitle>{t("browserSavedContentAlertTitle")}</AlertTitle>
           <AlertDescription>{t("browserSavedContentAlertDescription")}</AlertDescription>
