4d6a6ff81d2f079295aa97c4c9e33af6701bc064
Levente <168279599+tothlevente@users.noreply.github.com>
2025-03-27 08:45:23 +0000

    Refactor todoService to use TaskProps and update related functions for terminology consistency

--- src/services/todoService.ts
+++ src/services/todoService.ts
@@ -1,31 +1,13 @@
-import { TodoProps } from "@/interfaces/TodoProps";
+import { TaskProps } from "@/types/TaskProps";
 
-/**
- * The key used to store and retrieve the to-do list items from local storage.
- */
-const key = "todos";
+const key = "tasks";
 
-/**
- * Retrieves an item from local storage and parses it as JSON.
- *
- * @returns {any[]} The parsed JSON object if the item exists, otherwise an empty array.
- */
-export const getTodos = () => {
+export const getTasks = () => {
   return localStorage.getItem(key) !== null ? JSON.parse(localStorage.getItem(key)!) : [];
 };
 
-/**
- * Sets the local storage with the provided todo items and a new todo item.
- *
- * @param todos - The current list of todo items.
- * @param id - The unique identifier for the new todo item.
- * @param title - The title of the new todo item.
- * @param color - The color associated with the new todo item.
- * @param completed - The completion status of the new todo item.
- * @param createdAt - The creation date of the new todo item.
- */
-export const createTodo = (
-  todos: TodoProps[],
+export const createTasks = (
+  tasks: TaskProps[],
   id: number,
   title: string,
   color: string,
@@ -35,7 +17,7 @@ export const createTodo = (
   localStorage.setItem(
     key,
     JSON.stringify([
-      ...todos,
+      ...tasks,
       {
         id: id,
         title: title,
@@ -47,20 +29,10 @@ export const createTodo = (
   );
 };
 
-/**
- * Updates the local storage with the provided list of todos.
- *
- * @param {TodoProps[]} todos - The array of todo items to be stored in local storage.
- */
-export const updateTodos = (todos: TodoProps[]) => {
-  localStorage.setItem(key, JSON.stringify(todos));
+export const updateTasks = (tasks: TaskProps[]) => {
+  localStorage.setItem(key, JSON.stringify(tasks));
 };
 
-/**
- * Deletes an item from the local storage.
- *
- * @param {string} key - The key of the item to be removed from local storage.
- */
-export const deleteTodos = () => {
+export const deleteTasks = () => {
   localStorage.removeItem(key);
 };
