b70eda29e8b1f8ef587acbd09ef633dcbc35a61e
Levente <168279599+tothlevente@users.noreply.github.com>
2025-03-27 09:58:46 +0000

    Refactor context naming conventions for consistency in TasksContext

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -1,26 +1,26 @@
 import { TaskProps } from "@/types/TaskProps";
 import { createContext, useContext, useState } from "react";
 
-interface tasksContextProps {
+interface TasksContextProps {
   tasks: TaskProps[];
   setTasks: React.Dispatch<React.SetStateAction<TaskProps[]>>;
 }
 
-export const tasksContext = createContext<tasksContextProps>({
+export const TasksContext = createContext<TasksContextProps>({
   tasks: [],
   setTasks: () => {},
 });
 
-export const tasksProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const TasksProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [tasks, setTasks] = useState<TaskProps[]>([]);
 
   return (
-    <tasksContext.Provider value={{ tasks, setTasks }}>{children}</tasksContext.Provider>
+    <TasksContext.Provider value={{ tasks, setTasks }}>{children}</TasksContext.Provider>
   );
 };
 
 export const useTasks = () => {
-  const context = useContext(tasksContext);
+  const context = useContext(TasksContext);
 
   if (!context) {
     throw new Error("useTasks must be used within a TasksProvider");
