248ea45b439daf157e4c22c8f11e1d76dedce61b
Levente <168279599+tothlevente@users.noreply.github.com>
2025-03-27 08:45:30 +0000

    Refactor downloadService to use TaskProps and update download functions for consistency

--- src/services/downloadService.ts
+++ src/services/downloadService.ts
@@ -1,15 +1,6 @@
-import { TodoProps } from "@/interfaces/TodoProps";
+import { TaskProps } from "@/types/TaskProps";
 
-/**
- * Downloads the list of todos as a text file.
- *
- * This function takes an array of `TodoProps` objects, extracts their titles,
- * concatenates them into a single string separated by commas, and triggers
- * a download of this string as a text file named "myTodoListFile.txt".
- *
- * @param value - An array of `TodoProps` objects representing the todos.
- */
-const downloadTodosAsText = (value: TodoProps[]): void => {
+export const downloadTaskAsText = (value: TaskProps[]): void => {
   const separator = ", ";
   let result = "";
 
@@ -26,33 +17,20 @@ const downloadTodosAsText = (value: TodoProps[]): void => {
   });
 
   element.href = URL.createObjectURL(file);
-  element.download = "myTodoListFile.txt";
+  element.download = `tasks_${Date.now()}.json`;
   document.body.appendChild(element);
   element.click();
 };
 
-/**
- * Downloads the provided list of todos as a JSON file.
- *
- * @param value - An array of TodoProps objects to be downloaded as JSON.
- *
- * This function converts the provided array of todos into a JSON string,
- * creates a Blob from the JSON string, and generates a temporary URL for the Blob.
- * It then creates an anchor element, sets its href attribute to the Blob URL,
- * and triggers a download by programmatically clicking the anchor element.
- * Finally, it revokes the Blob URL to free up resources.
- */
-const downloadTodosAsJson = (value: TodoProps[]): void => {
+export const downloadTaskAsJson = (value: TaskProps[]): void => {
   const json = JSON.stringify(value, null, 2);
   const blob = new Blob([json], { type: "application/json" });
   const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
 
   a.href = url;
-  a.download = "myTodoListFile.json";
+  a.download = `tasks_${Date.now()}.json`;
   a.click();
 
   URL.revokeObjectURL(url);
 };
-
-export { downloadTodosAsText, downloadTodosAsJson };
