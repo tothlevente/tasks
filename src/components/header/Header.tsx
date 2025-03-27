f023278ad6ac6159e81d45bdb5eed7dbc15a3034
Levente <168279599+tothlevente@users.noreply.github.com>
2025-03-27 09:44:01 +0000

    Refactor Header component to use tasks context and update task handling functions

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,30 +1,29 @@
 import { useDefaultColor } from "@/context/DefaultColorContext";
+import { CirclePlusIcon, NotepadTextIcon } from "lucide-react";
 import { useUserInput } from "@/context/UserInputContext";
-import { useTodoList } from "@/context/TodoListContext";
-import { createTodo } from "@/services/todoService";
+import { createTasks } from "@/services/todoService";
+import { useTasks } from "@/context/TasksContext";
 import { ModeToggle } from "../themes/ModeToggle";
 import { Settings } from "../settings/Settings";
-import { CirclePlusIcon } from "lucide-react";
-import { ContentInput } from "./ContentInput";
-import { HeaderTitle } from "./HeaderTitle";
+import { HeaderInput } from "./HeaderInput";
 import { Button } from "../ui/button";
 
 export const Header = () => {
   const { userInput, setUserInput } = useUserInput();
-  const { todoList, setTodoList } = useTodoList();
   const { defaultColor } = useDefaultColor();
+  const { tasks, setTasks } = useTasks();
 
   const maxLength = 50;
 
-  function handleAddTodo() {
+  function handleAddTasks() {
     const id: number = Math.round(Math.random() * 10000000);
     const title: string = userInput;
     const completed: boolean = false;
     const createdAt: Date = new Date();
     const color = defaultColor;
 
-    const todos = [
-      ...todoList,
+    const taskValue = [
+      ...tasks,
       {
         id: id,
         title: title,
@@ -34,17 +33,20 @@ export const Header = () => {
       },
     ];
 
-    setTodoList(todos);
-    createTodo(todoList, id, title, color, completed, createdAt);
+    setTasks(taskValue);
+    createTasks(tasks, id, title, color, completed, createdAt);
     setUserInput("");
   }
 
   return (
     <div className="header">
-      <HeaderTitle />
+      <div className="header-title">
+        <NotepadTextIcon />
+        <p>Tasks</p>
+      </div>
       <div className="header-container">
-        <ContentInput
-          addTodo={handleAddTodo}
+        <HeaderInput
+          addTasks={handleAddTasks}
           maxLength={maxLength}
         />
         <div className="header-min-container">
@@ -52,7 +54,7 @@ export const Header = () => {
             variant="outline"
             size="icon"
             style={{ marginRight: "5px" }}
-            onClick={() => handleAddTodo()}
+            onClick={() => handleAddTasks()}
             disabled={userInput.length === 0 || userInput.length > maxLength}
           >
             <CirclePlusIcon />
